74c70c2c4e0fdb3eb8bcaa316231818e5cf1d926
Max Melnikov <mml@highcore.io>
2022-10-04 22:36:42 +0300

    added some plugins

---
+++ Assets/Plane.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Playables;
+
+public class Plane : MonoBehaviour
+{
+    [SerializeField] private float _t;
+    [SerializeField] private PathCreator _pathCreator;
+ 
+    private void Update()
+    {
+        UpdateTransform();
+    }
+
+    public void UpdateTransform()
+    {
+        transform.position = _pathCreator.path.GetPointAtTime(_t);
+        transform.rotation = _pathCreator.path.GetRotation(_t);
+    }
+}
