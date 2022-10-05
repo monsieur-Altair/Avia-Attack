f769aaff39a7a8fa945dd51b0e6d5330401c8a5d
Max Melnikov <mml@highcore.io>
2022-10-05 14:20:52 +0300

    added cinemachine

---
+++ Assets/Scripts/MotionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PathCreation;
+using UnityEngine;
+
+[Serializable]
+public class MotionInfo
+{
+    [SerializeField] private float _t;
+    [SerializeField] private PathCreator _pathCreator;
+
+    public float T => _t;
+    public PathCreator PathCreator => _pathCreator;
+}
---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Plane : TimelineObject
+{
+    [SerializeField] private int _currentSceneIndex;
+    [SerializeField] private MotionInfo[] _motionInfos;
+
+    private MotionInfo Info => _motionInfos[_currentSceneIndex];
+    
+    private void UpdateTransform()
+    {
+        transform.position = Info.PathCreator.path.GetPointAtTime(Info.T);
+        transform.rotation = Info.PathCreator.path.GetRotation(Info.T);
+    }
+
+    protected override void SceneView_DuringSceneGui(SceneView obj)
+    {
+        base.SceneView_DuringSceneGui(obj);
+        
+        UpdateTransform();
+    }
+}
