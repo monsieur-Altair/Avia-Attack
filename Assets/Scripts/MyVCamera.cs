5c70ae49992be262df3f23649c8afd1c4dcc45db
Max Melnikov <mml@highcore.io>
2022-10-06 20:07:44 +0300

    created smooth transition to scene 2

---
+++ Assets/Scripts/MyVCamera.cs
@@ -0,0 +1,16 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MyVCamera : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera _camera;
+        
+        public void DisableTranspose()
+        {
+            Debug.LogError("disabling");
+            _camera.Follow = null;
+        }
+    }
+}
