9d71ab7744a995fe7084d10b9976656b70986102
Max Melnikov <mml@highcore.io>
2022-10-23 18:37:58 +0300

    added foam

---
+++ Assets/Scripts/mover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class mover : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        private void Update()
+        {
+            transform.position += Vector3.forward * _speed * Time.deltaTime;
+        }
+    }
+}
