f6b3792e43c80616552675cc2c2dd70987d50cc4
Max Melnikov <mml@highcore.io>
2022-10-30 21:40:54 +0300

    added smoke again and fix one bug

--- Assets/Scripts/PlaneNoRot.cs
+++ Assets/Scripts/PlaneNoRot.cs
@@ -2,6 +2,7 @@
 {
     protected override void UpdateTransform()
     {
-        transform.position = _pathCreators[_currentSceneIndex].path.GetPointAtTime(_t);
+        if(gameObject.activeSelf)
+            transform.position = _pathCreators[_currentSceneIndex].path.GetPointAtTime(_t);
     }
 }
