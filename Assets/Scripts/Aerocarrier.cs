e0d6e83b743efe26d794c3e3514c5b89c595afda
Max Melnikov <mml@highcore.io>
2022-11-19 23:27:30 +0300

    aaa

--- Assets/Scripts/Aerocarrier.cs
+++ Assets/Scripts/Aerocarrier.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DG.Tweening;
 using Extensions;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -11,7 +12,7 @@ namespace DefaultNamespace
         [SerializeField] private Material _newMat;
         [SerializeField] private int _index = 11;
         [SerializeField] private AudioSource _machineGunSound;
-
+        private int _index1 = -1;
         private void Awake()
         {
             _machineGunSound.loop = true;
@@ -43,6 +44,22 @@ namespace DefaultNamespace
             Material[] materials = _meshRenderer.sharedMaterials;
             materials[_index] = _newMat;
             _meshRenderer.sharedMaterials = materials;
+        }
+
+        public void OnSceneSwitched()
+        {
+            _index1++;
+
+            if (_index1 == 5)
+            {
+                _machineGunSound.enabled = false;
+            }
+            
+            if (_index1 == 6)
+            {
+                _machineGunSound.enabled = true;
+            }
+
         }
     }
 }
