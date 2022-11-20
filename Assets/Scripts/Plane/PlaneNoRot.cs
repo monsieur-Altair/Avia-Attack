db14ad3a16c66767797cd307d157d0c123fce997
Max Melnikov <mml@highcore.io>
2022-11-20 18:54:51 +0300

    ended plane breaking

--- Assets/Scripts/Plane/PlaneNoRot.cs
+++ Assets/Scripts/Plane/PlaneNoRot.cs
@@ -2,7 +2,7 @@
 {
     protected override void UpdateTransform()
     {
-        if(gameObject.activeSelf)
+        if(gameObject.activeSelf && _pathCreators[_currentSceneIndex] != null)
             transform.position = _pathCreators[_currentSceneIndex].path.GetPointAtTime(_t);
     }
 }
